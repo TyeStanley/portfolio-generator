589c3a1041e8e4f7ea599aae671bac430a4b936f
Tye Stanley <tyestanley090@gmail.com>
2022-05-07 23:16:58 -0500

    modularized the generatePage function

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs;
 
-console.log(profileDataArgs);
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
 
-const printProfileData = profileDataArr => {
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
